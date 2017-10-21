01a025d93553a2a1d835c1c130511ed1606a887f
Austin Tanner <austin.tanner04@gmail.com>
2017-10-21 12:14:22 -0500

    added describe so parse test

--- server/server.test.js
+++ server/server.test.js
@@ -3,24 +3,30 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it("should return 'hey there'", (done) => {
-    request(app)
-      .get('/')
-      .expect(404)
-      .expect((res) => {
-         expect(res.body).toInclude({
-             error: 'page not found'
-         });
-      })
-      .end(done);
-});
+describe('Server', () => {
+    describe('#/', () => {
+        it("should return 'hey there'", (done) => {
+            request(app)
+              .get('/')
+              .expect(404)
+              .expect((res) => {
+                 expect(res.body).toInclude({
+                     error: 'page not found'
+                 });
+              })
+              .end(done);
+        });
+    });
 
-it("should return the users table", (done) => {
-    request(app)
-      .get('/users')
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toInclude({name: 'Austin', age:24});
-      })
-      .end(done);
+    describe('#/users', () => {
+        it("should return the users table", (done) => {
+            request(app)
+              .get('/users')
+              .expect(200)
+              .expect((res) => {
+                expect(res.body).toInclude({name: 'Austin', age:24});
+              })
+              .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,55 +1,65 @@
 const utils = require('./utils')
 const expect = require('expect');
 
-it('should add 2 numbers', () => {
-    var res = utils.add(9,1);
-    expect(res).toBe(10).toBeA('number');
 
-    // if(res !== 10){
-    //     throw new Error(`Expected 10 got ${res}`);
-    // }
-});
+describe('Utils', () => {
+    describe('#add', () =>{
+        it('should add 2 numbers', () => {
+            var res = utils.add(9,1);
+            expect(res).toBe(10).toBeA('number');
+        });
 
-it('should square a number', () => {
-    var res = utils.square(9);
-
-    expect(res).toBe(81).toBeA('number');
-
-    // if(res !== 81){
-    //     throw new Error(`Expected 81 got ${res}`);
-    // }
-});
-
-it("should verify a user's first and last name", () => {
-    var user = {
-        location: 'St. Louis',
-        age: 24
-    }
-
-    utils.setName(user, 'Austin Tanner');
-
-    expect(user).toInclude({
-        firstName: 'Austin',
-        lastName: 'Tanner'
-    }).toBeA('object');
-});
-
-// it('should be a thing', () => {
-//     // expect(12).toNotBe(11);
-//
-//
-// });
-
-it("should async add 2 numbers", (done) => {
-    utils.aSyncAdd(1, 1, (sum) => {
-        expect(sum).toBe(2).toBeA('number');
-        done();
+        it("should async add 2 numbers", (done) => {
+            utils.aSyncAdd(1, 1, (sum) => {
+                expect(sum).toBe(2).toBeA('number');
+                done();
+            });
+        });
     });
-});
 
-it("should square a number", (done) => {
-    utils.aSyncSquare(2, (square) => {
-        expect(square).toBe(4).toBeA('number');
-        done();
+    describe('#square', () => {
+        it('should square a number', () => {
+            var res = utils.square(9);
+
+            expect(res).toBe(81).toBeA('number');
+
+            // if(res !== 81){
+            //     throw new Error(`Expected 81 got ${res}`);
+            // }
+        });
+
+        it("should square a number", (done) => {
+            utils.aSyncSquare(2, (square) => {
+                expect(square).toBe(4).toBeA('number');
+                done();
+            });
+        });        
     });
-})
+
+        // if(res !== 10){
+        //     throw new Error(`Expected 10 got ${res}`);
+        // }
+
+
+
+
+    it("should verify a user's first and last name", () => {
+        var user = {
+            location: 'St. Louis',
+            age: 24
+        }
+
+        utils.setName(user, 'Austin Tanner');
+
+        expect(user).toInclude({
+            firstName: 'Austin',
+            lastName: 'Tanner'
+        }).toBeA('object');
+    });
+
+    // it('should be a thing', () => {
+    //     // expect(12).toNotBe(11);
+    //
+    //
+    // });
+});
